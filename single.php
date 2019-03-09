e8b62c00996a574986a74c30a9bf576c3d94d96a
Dustin Jones <info@domesticjones.com>
2019-03-08 18:45:41 -0700

    Restyle single blog page

--- single.php
+++ single.php
@@ -5,12 +5,10 @@
     echo '<article class="blog-single">';
       while(have_posts()): the_post();
 ?>
-  <header class="blog-single-hero" style="background-image: url(<?php the_post_thumbnail_url('jumbo'); ?>)">
-    <?php the_post_thumbnail('jumbo'); ?>
-    <div class="blog-single-hero-data">
-      <h1><?php the_title(); ?></h1>
-      <h2><?php ex_post_meta('', ''); ?></h2>
-    </div>
+  <header class="blog-single-hero">
+    <h1><?php the_title(); ?></h1>
+    <h2><?php ex_post_meta('', ''); ?></h2>
+    <?php the_post_thumbnail('large'); ?>
   </header>
   <section class="blog-single-content">
     <?php the_content(); ?>
